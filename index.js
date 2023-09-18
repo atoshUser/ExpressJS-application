6e02e8c5b2dcb12e30f930129c8370c88b6ac614
Abdurahmon <abdurahmonavezdjanov031@gmail.com>
2023-09-18 22:11:46 +0500

    Configurated handlebars

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
+import { create } from "express-handlebars";
 import express from "express";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+
 const app = express();
 
-// File name __filename yaratish
-const __filename = fileURLToPath(import.meta.url);
-console.log(__filename);
+const hbs = create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
 
-// Folderni ko'rsatish uchun __dirname yaratish
-const __dirname = dirname(__filename);
-console.log(__dirname);
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "index.html"));
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "about.html"));
-});
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
