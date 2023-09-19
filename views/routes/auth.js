6fff1d7e8047d7410e53a49a91b0420596f349a4
Abdurahmon <abdurahmonavezdjanov031@gmail.com>
2023-09-19 12:53:31 +0500

    add login & register

--- views/routes/auth.js
+++ views/routes/auth.js
@@ -2,11 +2,21 @@ import { Router } from "express";
 
 const router = Router();
 router.get("/login", (req, res) => {
-    res.render("login");
-  });
-  
-  router.get("/register", (req, res) => {
-    res.render("register");
-  });
+  res.render("login", { title: "Login | Page" });
+});
 
-  export default router
+router.get("/register", (req, res) => {
+  res.render("register", { title: "Register | Page" });
+});
+
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+export default router;
--- views/routes/product.js
+++ views/routes/product.js
@@ -2,15 +2,15 @@ import { Router } from "express";
 
 const router = Router();
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Main | Atosh" });
 });
 
 router.get("/products", (req, res) => {
-  res.render("products");
+  res.render("products", { title: "Products | Page", isProduct:true });
 });
 
 router.get("/add", (req, res) => {
-  res.render("add");
+  res.render("add", { title: "Add | Page", isAdd:true });
 });
 
 export default router;
