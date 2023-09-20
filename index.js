d7011e899f63d504b833160f27e2d51f0bfbfe93
Abdurahmon <abdurahmonavezdjanov031@gmail.com>
2023-09-20 15:44:08 +0500

    add connect-flash & sessioin & validation auth

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { create } from "express-handlebars";
 import express from "express";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+import flash from "connect-flash";
+import session from "express-session";
 // Routes
 import AuthRoutes from "./views/routes/auth.js";
 import ProductsRoutes from "./views/routes/product.js";
@@ -19,9 +21,11 @@ app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.json())
-
-app.use(AuthRoutes)
+app.use(express.json());
+app.use(flash());
+app.use(session({secret:'atosh',resave:false,saveUninitialized:false}))
+// routes
+app.use(AuthRoutes);
 app.use(ProductsRoutes);
 
 const startProject = () => {
--- views/routes/auth.js
+++ views/routes/auth.js
@@ -3,38 +3,68 @@ import User from "../../models/user.js";
 import bcrypt from "bcrypt";
 const router = Router();
 router.get("/login", (req, res) => {
-  res.render("login", { title: "Login | Page" });
+  res.render("login", {
+    title: "Login | Page",
+    isLogin: true,
+    loginError: req.flash("loginError"),
+  });
 });
 
 router.get("/register", (req, res) => {
-  res.render("register", { title: "Register | Page" });
+  res.render("register", {
+    title: "Register | Page",
+    isRegister: true,
+    registerError: req.flash("registerError"),
+  });
 });
 
 router.post("/login", async (req, res) => {
-  const existUser = await User.findOne({ email: req.body.Email });
+  const { Email, Password } = req.body;
+
+  if (!Password || !Email) {
+    req.flash("loginError", "You must complete complete all requests");
+    res.redirect("/login");
+    return;
+  }
+
+  const existUser = await User.findOne({ email: Email });
   if (existUser) {
-    const isPasswordEqual = await bcrypt.compare(
-      req.body.Password,
-      existUser.password
-    );
+    const isPasswordEqual = await bcrypt.compare(Password, existUser.password);
     if (isPasswordEqual) {
       console.log(existUser);
     } else {
-      console.log("Password is not correct");
+      req.flash("loginError", "Password is not correct");
+      res.redirect("/login");
+      return;
     }
   } else {
-    console.log("User not found");
+    req.flash("loginError", "Email is not correct");
+    res.redirect("/login");
+    return;
   }
 
   res.redirect("/");
 });
 
 router.post("/register", async (req, res) => {
-  const hashCode = await bcrypt.hash(req.body.Password, 10);
+  const { userName, userSurname, Email, Password } = req.body;
+  if (!userSurname || !userName || !Password || !Email) {
+    req.flash("registerError", "You must complete all fields!");
+    res.redirect("/register");
+    return;
+  }
+
+  const condidate = User.findOne({ email: Email });
+  if (condidate) {
+    req.flash("registerError", "User already exist");
+    res.redirect("/register");
+    return;
+  }
+  const hashCode = await bcrypt.hash(Password, 10);
   const userData = {
-    firstName: req.body.userName,
-    lastName: req.body.userSurname,
-    email: req.body.Email,
+    firstName: userName,
+    lastName: userSurname,
+    email: Email,
     password: hashCode,
   };
   await User.create(userData);
