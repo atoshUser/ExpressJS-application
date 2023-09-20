4c96590eda6dc19059caf64889243ad0928667eb
Abdurahmon <abdurahmonavezdjanov031@gmail.com>
2023-09-20 12:41:14 +0500

    add bcrypt library and hash user password

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { create } from "express-handlebars";
 import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+// Routes
 import AuthRoutes from "./views/routes/auth.js";
 import ProductsRoutes from "./views/routes/product.js";
+dotenv.config();
 const app = express();
 
 const hbs = create({
@@ -15,12 +19,21 @@ app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json())
 
-app.use(AuthRoutes);
+app.use(AuthRoutes)
 app.use(ProductsRoutes);
 
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+const startProject = () => {
+  try {
+    const PORT = process.env.PORT || 8000;
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => {
+      console.log(`Mongo DB created`);
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  } catch (error) {}
+};
+startProject();
--- views/routes/auth.js
+++ views/routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-
+import User from "../../models/user.js";
+import bcrypt from "bcrypt";
 const router = Router();
 router.get("/login", (req, res) => {
   res.render("login", { title: "Login | Page" });
@@ -14,8 +15,15 @@ router.post("/login", (req, res) => {
   res.redirect("/");
 });
 
-router.post("/register", (req, res) => {
-  console.log(req.body);
+router.post("/register", async (req, res) => {
+  const hashCode = await bcrypt.hash(req.body.Password, 10);
+  const userData = {
+    firstName: req.body.userName,
+    lastName: req.body.userSurname,
+    email: req.body.Email,
+    password: hashCode,
+  };
+  await User.create(userData);
   res.redirect("/");
 });
 
